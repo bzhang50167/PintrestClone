eef8a84152f8fd83a58522cd6fa1e8c42a942969
Bao <bao4ltyfe@gmail.com>
2023-05-22 08:32:16 -0700

    just saving

--- react-app/src/components/EditUserModal/index.js
+++ react-app/src/components/EditUserModal/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { AiFillSmile } from "react-icons/ai";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { editUserThunk } from "../../store/session";
 
 
@@ -8,13 +8,14 @@ const EditUserModal = (id) => {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [img, setImg] = useState(null)
+    const user = useSelector(state => state.session.user)
     const dispatch = useDispatch()
 
     const handleImage = (e) => {
         setImg(e.target.files[0])
     }
 
-    const handleSubmit = async(e) => {
+    const handleSubmit = async (e) => {
         const formData = new FormData()
         formData.append('first_name', firstName)
         formData.append('last_name', lastName)
@@ -22,42 +23,51 @@ const EditUserModal = (id) => {
 
         await dispatch(editUserThunk(+id.id, formData))
     }
-
+    console.log(user);
     return (
         <div>
+            <h1>EDIT PROFILE</h1>
             <form>
-                <label>
-                    First Name
-                    <input
-                        type="text"
-                        value={firstName}
-                        onChange={e => setFirstName(e.target.value)}
-                        placeholder="First Name"
-                    />
-                </label>
-                <label>
-                    Last Name
-                    <input
-                        type="text"
-                        value={lastName}
-                        onChange={e => setLastName(e.target.value)}
-                        placeholder="Last Name"
-                    />
-                </label>
-                <label>
-                    Profile Pic
-                    <input
-                        type="file"
-                        name="image"
-                        accept="image/*"
-                        onChange={handleImage}
-                    />
-                </label>
-                <button
-                    type="submit"
-                    onClick={handleSubmit}>
-                    <AiFillSmile />
-                </button>
+                <div className="editdiv">
+                    <label>
+                        First Name:
+                        <input
+                            type="text"
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                            placeholder={user.firstName}
+                        />
+                    </label>
+                </div>
+                <div className="editdiv">
+                    <label>
+                        Last Name:
+                        <input
+                            type="text"
+                            value={lastName}
+                            onChange={e => setLastName(e.target.value)}
+                            placeholder={user.lastName}
+                        />
+                    </label>
+                </div>
+                <div className="editdiv">
+                    <label>
+                        Profile Pic:
+                        <input
+                            type="file"
+                            name="image"
+                            accept="image/*"
+                            onChange={handleImage}
+                        />
+                    </label>
+                </div>
+                <div className="editdiv">
+                    <button
+                        type="submit"
+                        onClick={handleSubmit}>
+                        <AiFillSmile />
+                    </button>
+                </div>
             </form>
         </div>
     )
--- react-app/src/components/Posts/AllPost.js
+++ react-app/src/components/Posts/AllPost.js
@@ -6,19 +6,26 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Masonry from "react-masonry-css";
 
 const AllPost = () => {
+    const sessionUser = useSelector(state => state.session.user)
     const allPost = useSelector(state => state.post.allPost)
-    const deletePost = useSelector(state => state.post.delete)
+    // const deletePost = useSelector(state => state.post.delete)
     const posts = Object.values(allPost)
     // const deleted = Object.values(deletePost)
     const history = useHistory()
     const dispatch = useDispatch()
     // const [postLength, setPostLength] = useState(0)
-    console.log(posts.length);
+    // console.log(posts.length);
     // console.log(postLength);
 
     useEffect(() => {
         dispatch(getAllPostThunk())
     }, [dispatch, posts.length])
+
+    console.log(sessionUser);
+
+    if(!sessionUser){
+        history.push('/')
+    }
 
 
     const breakpointColumnsObj = {
