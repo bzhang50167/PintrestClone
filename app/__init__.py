9140b6bbf1d8aaa2553c9cdaf4822a58977febc4
Bao <bao4ltyfe@gmail.com>
2023-05-24 17:02:55 -0700

    ss

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,6 @@ from .api.post_routes import post_routes
 from .api.group_routes import group_routes
 from .seeds import seed_commands
 from .config import Config
-from .socket import socketio
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
@@ -39,7 +38,6 @@ db.init_app(app)
 Migrate(app, db)
 
 
-socketio.init_app(app)
 # Application Security
 CORS(app)
 
@@ -98,7 +96,3 @@ def react_root(path):
 @app.errorhandler(404)
 def not_found(e):
     return app.send_static_file('index.html')
-
-
-if __name__ == '__main__':
-    socketio.run(app)
